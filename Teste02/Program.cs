448d7a28ecd4fdce35c47828c0f52b7a9ff722e9
murilojcavalcanti <josemurilo7@gmail.com>
2023-03-01 22:34:55 -0300

    Teste Concluido

--- Teste02/Program.cs
+++ Teste02/Program.cs
@@ -1,4 +1,6 @@
-﻿//Qustão 2:
+﻿using System;
+
+//Qustão 2:
 /*
  Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores 
  (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, 
@@ -9,17 +11,36 @@ Esse número pode ser informado através de qualquer entrada de sua preferência
 */
 int Fibonacci=1;
 int numeroAnterior=0;
+List<int> listaFibonacci = new List<int>();
+bool pertence=false;
+
 
 System.Console.WriteLine("Digite um valor");
 int.TryParse(Console.ReadLine(), out int valor);
 
+int contador=0;
 while(Fibonacci<=valor){
-Fibonacci=Fibonacci+numeroAnterior;
-System.Console.WriteLine(Fibonacci);
-numeroAnterior++;
+    Fibonacci=Fibonacci+numeroAnterior;
+    System.Console.WriteLine(Fibonacci);
+
+    listaFibonacci.Add(Fibonacci);
+
+    numeroAnterior++;
+    contador++;
 }
 
-if(Fibonacci==valor){
+foreach (int fibo in listaFibonacci)
+{
+if(fibo==valor){
+    pertence=true;
+    break;
+}else{
+    pertence=false;
+}
+
+}
+
+if(pertence==true){
     System.Console.WriteLine($"O valor {valor} pertence a sequencia de fibonacci");
 }else{
     System.Console.WriteLine($" O valor {valor} não pertence a sequencia de fibonacci");
